c152ee18b940b7e6bf4951de62364a4ea9676169
Jinseok Lee <sonaky47@gmail.com>
2019-02-11 17:05:23 +0900

    Members model > insertMember sample code

--- models/Members.js
+++ models/Members.js
@@ -8,6 +8,25 @@ class Members extends Model {
   static get tableName () {
     return 'Members';
   }
+
+  async insertMember (member) {
+    let connection = null;
+    try {
+      connection = await super.getConnection();
+    } catch (err) {
+      throw new Error(err);
+    }
+
+    let sql = `INSERT INTO ${Members.tableName} (email, pwd, reg_date) VALUES ('${member.email}', '${member.pwd}')`;
+    let result = null;
+    try {
+      result = await connection.query(sql);
+    } catch (err) {
+      throw new Error(err);
+    }
+
+    return result;
+  }
 }
 
 module.exports = Members;
